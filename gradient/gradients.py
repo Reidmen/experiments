fixed error with conjugate gradient

gradient/gradients.py
@@ -1,11 +1,8 @@
-from __future__ import annotations
 import numpy as np
 from numpy.typing import NDArray
 from typing import TypeAlias
 
-NDarray16: TypeAlias = NDArray[np.float16]
 NDarray32: TypeAlias = NDArray[np.float32]
-NDarray64: TypeAlias = NDArray[np.float64]
 
 
 def laplacian_operator(
@@ -16,7 +13,7 @@ def laplacian_operator(
             -4.0 * array[1:-1, 1:-1]
             + array[1:-1, :-2]
             + array[1:-1, 2:]
-            + array[:2, 1:-1]
+            + array[:-2, 1:-1]
             + array[2:, 1:-1]
         )
         / dx
@@ -72,22 +69,22 @@ def ConjugateGradientDescent(
 ) -> tuple[NDarray32, int, list[np.float32]]:
     solution: NDarray32 = array.copy()
     residual: NDarray32 = np.zeros_like(solution)
-    direction: NDarray32 = np.zeros_like(solution)
     operator_dot_direction: NDarray32 = np.zeros_like(solution)
     compute_residual(residual, solution, rhs)  # initial residual
+    direction: NDarray32 = residual.copy()
 
     history: list[np.float32] = []
     diff: np.float32 = np.float32(1e8)
     i: int = 0
     while diff > relative_tol and i < max_iter:
-        solution_k = solution.copy()
+        solution_k: NDarray32 = solution.copy()
         residual_k: NDarray32 = residual.copy()
         compute_laplacian_on_array(operator_dot_direction, direction)
-        alpha = np.dot(residual, residual) / np.dot(
-            direction, operator_dot_direction
+        alpha = np.sum(residual * residual) / np.sum(
+            direction * operator_dot_direction
         )
         # update solution and residual
-        solution = solution_k + alpha * residual
+        solution = solution_k + alpha * direction
         residual = residual_k - alpha * operator_dot_direction
         # update descent direction
         beta = np.sum(residual * residual) / np.sum(residual_k * residual_k)
@@ -108,7 +105,7 @@ def compute_norm_to_reference(
     l2_difference = np.sqrt(np.sum((solution - reference) ** 2))
     l2_reference = np.sqrt(np.sum(reference**2))
 
-    if l2_difference < atol:
+    if l2_difference < atol or l2_reference < atol:
         return l2_difference
     else:
         return l2_difference / l2_reference
@@ -138,20 +135,22 @@ def test_conjugate_gradient() -> None:
     solution, iterations, history = ConjugateGradientDescent(
         solution, rhs, max_iter, rtol
     )
-    print(f"CG converged in {iterations} "
-          f"with relative tolerance {history[-1]}")
+    print(
+        f"CG converged in {iterations} "
+        f"with relative tolerance {history[-1]}\n"
+    )
 
 
 def test_gradient_descent() -> None:
     X, Y, rhs, solution = create_mesh_and_rhs(0.01, 0.01, 1.0, 1.0)
-    max_iter, rtol = 1000, 1e-8
+    max_iter, rtol = 10000, 1e-6
 
-    solution, iterations, history = ConjugateGradientDescent(
+    solution, iterations, history = SteppestDescent(
         solution, rhs, max_iter, rtol
     )
     print(
         f"Gradient Descent converged in {iterations} "
-        f"with relative tolerance {history[-1]}"
+        f"with relative tolerance {history[-1]}\n"
     )
 
 
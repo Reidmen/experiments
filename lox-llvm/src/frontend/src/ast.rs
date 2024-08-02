977c90261eb3f7bbab3e5ec6c77e16845ed13a64
reidmen <r.rethmawn@gmail.com>
2024-08-02 15:17:30 +0200

    ast

--- lox-llvm/src/frontend/src/ast.rs
+++ lox-llvm/src/frontend/src/ast.rs
@@ -1,1 +1,49 @@
-use std::iter::Peekable
+use crate::scanner::TokenKind;
+
+/** Abstact Syntax Tree **/
+#[derive(Debug, PartialEq, Clone)]
+pub enum Literal {
+    True,
+    False,
+    Nil,
+    String(String),
+    Number(f64),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Operator {
+    Equal,
+    NotEqual,
+    Less,
+    LessOrEqual,
+    Greater,
+    GreaterOrEqual,
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum UnaryOperator {
+    Not,
+    Minus,
+}
+
+pub enum Expression {
+    Literal(Literal),
+    Unary(UnaryOperator, Box<Expression>),
+    Binary(Box<Expression>, Operator, Box<Expression>),
+    Grouping(Box<Expression>),
+    Variable(String),
+}
+
+impl From<&TokenKind> for UnaryOperator {
+    fn from(token: &TokenKind) -> Self {
+        match token {
+            TokenKind::Bang => UnaryOperator::Not,
+            TokenKind::Minus => UnaryOperator::Minus,
+            _ => panic!("can't convert {:?} into UnaryOperator", token),
+        }
+    }
+}
--- lox-llvm/src/frontend/src/main.rs
+++ lox-llvm/src/frontend/src/main.rs
@@ -1,3 +1,4 @@
+mod ast;
 mod scanner;
 
 fn main() {
